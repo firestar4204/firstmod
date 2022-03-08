37cf8f99a9d0976ef1d665cde1f01e82a4ff4102
Gabriel Mazzacane <firestar4204@outlook.com>
2022-03-08 17:20:47 -0500

    Add mud_ball item

--- src/main/java/io/github/firestar4204/firstmod/FirstMod.java
+++ src/main/java/io/github/firestar4204/firstmod/FirstMod.java
@@ -1,8 +1,13 @@
 package io.github.firestar4204.firstmod;
 
-import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class FirstMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
@@ -10,12 +15,16 @@ public class FirstMod implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LogManager.getLogger("modid");
 
+	public static final Item MUD_BALL = new Item(new Item.Settings().group(ItemGroup.MISC));
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		Registry.register(Registry.ITEM, new Identifier("firstmod", "mud_ball"), MUD_BALL);
+
+		LOGGER.info("FirstMod initialized.");
 	}
 }
