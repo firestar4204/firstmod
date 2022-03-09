54d7d3833684f3508a973ea9923303a0e593b0ad
Gabriel Mazzacane <firestar4204@outlook.com>
2022-03-09 09:22:01 -0500

    Add "Mud Block"

--- src/main/java/io/github/firestar4204/firstmod/FirstMod.java
+++ src/main/java/io/github/firestar4204/firstmod/FirstMod.java
@@ -4,8 +4,13 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -17,6 +22,8 @@ public class FirstMod implements ModInitializer {
 
 	public static final Item MUD_BALL = new Item(new Item.Settings().group(ItemGroup.MISC));
 
+	public static final Block MUD_BLOCK = new Block(FabricBlockSettings.of(Material.SOIL).strength(5.0F, 5.0F).sounds(BlockSoundGroup.GRAVEL));
+	
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -24,6 +31,8 @@ public class FirstMod implements ModInitializer {
 		// Proceed with mild caution.
 
 		Registry.register(Registry.ITEM, new Identifier("firstmod", "mud_ball"), MUD_BALL);
+		Registry.register(Registry.BLOCK, new Identifier("firstmod", "mud_block"), MUD_BLOCK);
+		Registry.register(Registry.ITEM, new Identifier("firstmod", "mud_block"), new BlockItem(MUD_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
 
 		LOGGER.info("FirstMod initialized.");
 	}
