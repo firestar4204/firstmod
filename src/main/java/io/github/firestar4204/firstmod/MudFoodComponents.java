58d4645b3277ae2cbd9170924896321c9f0857f6
firestar4204 <99219275+firestar4204@users.noreply.github.com>
2022-05-03 21:06:19 -0400

    Format MudFoodComponents.java

--- src/main/java/io/github/firestar4204/firstmod/MudFoodComponents.java
+++ src/main/java/io/github/firestar4204/firstmod/MudFoodComponents.java
@@ -6,5 +6,11 @@ import net.minecraft.item.FoodComponent;
 
 public class MudFoodComponents {
     
-    public static final FoodComponent DRIED_MUD_BALL = (new FoodComponent.Builder()).hunger(3).saturationModifier(0.35F).statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 600, 0), 0.75F).snack().build();
+    public static final FoodComponent DRIED_MUD_BALL = (new FoodComponent
+                                                        .Builder())
+                                                        .hunger(3)
+                                                        .saturationModifier(0.35F)
+                                                        .statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 600, 0), 0.75F)
+                                                        .snack()
+                                                        .build();
 }
